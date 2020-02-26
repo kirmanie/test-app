5b1db586fd0032649f2134c86c98425218fceede
Kirmanie L Ravariere <kirmanie.ravariere@nbcuni.com>
2020-02-26 17:05:52 -0500

    Create sample nodejs app

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+app.get(`/`, (req, res) => res.json({message: 'OK'}));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
